2e77419d2420625a411c1332b2aa941de7f1afb8
Dhruv Yadav <19387874+dhruvyad@users.noreply.github.com>
2025-09-03 19:56:15 +0530

    add stripe account topup
    Public-History-Instance: 13
    
    Signed-off-by: NoClick OSS Maintainers <opensource@noclick.com>

---
+++ frontend/app/components/ui/sonner.tsx
@@ -0,0 +1,35 @@
+// Sonner toast component for displaying dismissible notifications.
+// Positioned at bottom-right with proper theming support.
+
+import { Toaster as Sonner } from "sonner";
+
+type ToasterProps = React.ComponentProps<typeof Sonner>;
+
+const Toaster = ({ ...props }: ToasterProps) => {
+  return (
+    <Sonner
+      theme="dark"
+      className="toaster group"
+      position="bottom-right"
+      toastOptions={{
+        style: {
+          backgroundColor: '#282725',
+          borderColor: '#404040',
+          color: 'white'
+        },
+        classNames: {
+          toast:
+            "group toast group-[.toaster]:text-white group-[.toaster]:shadow-lg",
+          description: "group-[.toast]:text-gray-300",
+          actionButton:
+            "group-[.toast]:bg-white group-[.toast]:text-black",
+          cancelButton:
+            "group-[.toast]:bg-gray-700 group-[.toast]:text-gray-300",
+        },
+      }}
+      {...props}
+    />
+  );
+};
+
+export { Toaster };
