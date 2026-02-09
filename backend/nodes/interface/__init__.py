df6f259e4e9ae88e4e0d035a0ca89dcdc2e9e78c
Dhruv Yadav <19387874+dhruvyad@users.noreply.github.com>
2026-02-09 13:08:45 +0530

    add proper UX schemas
    Public-History-Instance: 879
    
    Signed-off-by: NoClick OSS Maintainers <opensource@noclick.com>

--- backend/nodes/interface/__init__.py
+++ backend/nodes/interface/__init__.py
@@ -3,8 +3,26 @@
 
 from nodes.interface.markdown_node import MarkdownInterfaceNode
 from nodes.interface.image_node import ImageInterfaceNode
+from nodes.interface.form_node import FormInterfaceNode
+from nodes.interface.audio_node import AudioInterfaceNode
+from nodes.interface.video_node import VideoInterfaceNode
+from nodes.interface.file_node import FileInterfaceNode
+from nodes.interface.dataframe_node import DataframeInterfaceNode
+from nodes.interface.plot_node import PlotInterfaceNode
+from nodes.interface.html_node import HtmlInterfaceNode
+from nodes.interface.file_upload_node import FileUploadInterfaceNode
+from nodes.interface.chatbot_node import ChatbotInterfaceNode
 
 __all__ = [
     'MarkdownInterfaceNode',
     'ImageInterfaceNode',
+    'FormInterfaceNode',
+    'AudioInterfaceNode',
+    'VideoInterfaceNode',
+    'FileInterfaceNode',
+    'DataframeInterfaceNode',
+    'PlotInterfaceNode',
+    'HtmlInterfaceNode',
+    'FileUploadInterfaceNode',
+    'ChatbotInterfaceNode',
 ]
